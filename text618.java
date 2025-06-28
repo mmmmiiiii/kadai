62ac7f5cad302d3756a072973dce9767ef5e850c
田中みのり <noiboshi7080@gmail.com>
2025-06-28 12:39:09 +0900

    初期版を作成

---
+++ text618.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class text618{
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        int[][] c;
+        System.out.print("行数: "); int n = stdIn.nextInt();
+        c = new int[n][];
+
+        for(int i = 0; i < n; i++){
+            System.out.print(i + "行目の要素数: "); int m = stdIn.nextInt();
+            c[i] = new int[m];
+        }
+
+        for(int i = 0; i < c.length; i++){
+            for(int j = 0; j < c[i].length; j++)
+                System.out.printf("%3d", c[i][j]);
+            System.out.println();
+        }
+    }
+}
